ed4ef7c50d0af5a1265529674a41a04a01881ef2
MariJ12 <mari.munizj@gmail.com>
2023-05-26 15:43:06 +0100

    Added Js moment js - time zone
    On Html deleted fake data

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,29 @@
+function updateTime() {
+  let brasilElement = document.querySelector("#idbrasil");
+  let brasilDateElement = document.querySelector("#date-brasil");
+  let brasilTimeElement = document.querySelector("#time-brasil");
+  let brasilZone = moment().tz("America/Sao_Paulo");
+  brasilDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  brasilTimeElement.innerHTML = brasilZone.format(
+    `H:m:SS [<small>]A[</small>]`
+  );
 
+  let tokyoElement = document.querySelector("#idtokyo");
+  let tokyoDateElement = document.querySelector("#date-tokyo");
+  let tokyoTimeElement = document.querySelector("#time-tokyo");
+  let tokyoZone = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoZone.format(`H:m:SS [<small>]A[</small>]`);
+
+  let sydneyElement = document.querySelector("#idsydney");
+  let sydneyDateElement = document.querySelector("#date-sydney");
+  let sydneyTimeElement = document.querySelector("#time-sydney");
+  let sydneyZone = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyZone.format(
+    `H:m:SS [<small>]A[</small>]`
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
